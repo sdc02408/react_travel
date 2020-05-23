0e21f03f749e5228a4c675cdd3c443f21d446214
chanseoky <chanseoky@naver.com>
2020-05-23 16:53:27 +0900

    멀티지도마커

--- client/src/components/views/Search/Sections/Map.js
+++ client/src/components/views/Search/Sections/Map.js
@@ -6,25 +6,31 @@ import { Col } from 'antd'
 
 function Map (props) {
   
-  console.log(props.mapy,"fffffffffffff")
-  console.log(props.mapx,"ccc")
-  console.log("finish")
-  
+
   const storeList = [
-    { name: 'CU', location: [37.565964, 126.986574] },
-    { name: '할리스', location: [37.564431, 126.986591] },
-    { name: '세븐일레븐', location: [37.565188, 126.983238] },
-    { name: '파리바게트', location: [37.564869, 126.984450] },
-    { name: '스타벅스', location: [37.562003, 126.985829] }
-  ];
+    { name: 'CU', location: [props.mapy, props.mapx] },
   
+  ];
+  console.log(storeList,"storelixe")
   
   return (
   
   
   <>
-  
-  
+    {
+      storeList.map((store, i) => (
+        <Marker
+          key={i}
+          latitude={store.location[0]}
+          longitude={store.location[1]}
+        >
+          <button
+            className="btn-marker"
+          />
+        </Marker>
+      ))
+    }
+    
   </>
   
   
